19d9cdb3bd1573d7c958261cf26700306335ce74
Hassan20057155 <ddgggdfhy@gmail.com>
2025-07-26 10:29:41 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rewan App',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Rewan App'),
+      ),
+      body: const Center(
+        child: Text('Hello from Rewan!'),
+      ),
+    );
+  }
+}
